2224ae95151771e50f4e650715d06a0cc735cdc0
masrur <you@example.com>
2024-01-12 04:01:31 +0500

    test

--- one/app.py
+++ one/app.py
@@ -1,35 +1,28 @@
-import requests
 import os
-import rarfile
+from zipfile import ZipFile
 
-def download_large_file(url, destination_path, chunk_size=8192):
-    with requests.get(url, stream=True) as response:
-        response.raise_for_status()
-        with open(destination_path, 'wb') as file:
-            for chunk in response.iter_content(chunk_size=chunk_size):
-                file.write(chunk)
+def split_zip(input_file, output_directory, chunk_size):
+    with ZipFile(input_file, 'r') as zip_archive:
+        file_size = os.path.getsize(input_file)
+        num_chunks = (file_size + chunk_size - 1) // chunk_size
 
-def create_rar_files(input_file_path, output_directory, num_files, file_size):
-    with rarfile.RarFile(input_file_path, 'r') as large_rar:
-        for i in range(num_files):
-            output_file_path = os.path.join(output_directory, f'output_file_{i + 1}.rar')
-            with rarfile.RarFile(output_file_path, 'w') as output_rar:
-                current_size = 0
-                while current_size < file_size:
-                    member = large_rar.namelist()[0]  # Assuming there is only one file in the large RAR
-                    data = large_rar.read(member, size=file_size - current_size)
-                    output_rar.writestr(member, data)
-                    current_size += len(data)
+        for i in range(num_chunks):
+            start = i * chunk_size
+            end = min((i + 1) * chunk_size, file_size)
+            chunk_name = f"{os.path.splitext(os.path.basename(input_file))[0]}_chunk{i + 1}.zip"
+            chunk_path = os.path.join(output_directory, chunk_name)
+
+            with ZipFile(chunk_path, 'w') as chunk_zip:
+                for file_info in zip_archive.infolist():
+                    with zip_archive.open(file_info.filename) as file_in_zip:
+                        chunk_zip.writestr(file_info, file_in_zip.read())
 
 if __name__ == "__main__":
-    large_file_url = "https://drive.usercontent.google.com"
-    large_file_destination = "./"
-    output_directory = "./"
-    num_files = 10
-    file_size = 1 * 1024 * 1024 * 1024  # 1.5 GB in bytes
+    input_file_path = "static/your_large_file.zip"  # Update with your actual file path
+    output_directory = "static/chunks"
+    chunk_size = 1024 * 1024 * 1024  # 1 GB in bytes
 
-    # Download the large file
-    download_large_file(large_file_url, large_file_destination)
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
 
-    # Create 10 RAR files
-    create_rar_files(large_file_destination, output_directory, num_files, file_size)
+    split_zip(input_file_path, output_directory, chunk_size)
